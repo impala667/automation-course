3f41e62b17d0857e18519d3a7e49caba1cd4b606
impala667 <musermasha@gmail.com>
2026-03-04 19:31:02 +0300

    Практическое задание №5

---
+++ src/test/java/testHomePageVisualTest.java
@@ -0,0 +1,44 @@
+import base.BaseTest;
+import com.github.romankh3.image.comparison.ImageComparison;
+import com.github.romankh3.image.comparison.ImageComparisonUtil;
+import com.github.romankh3.image.comparison.model.ImageComparisonResult;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.*;
+import org.junit.jupiter.api.Test;
+import com.microsoft.playwright.Page;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class testHomePageVisualTest extends BaseTest {
+    @Test
+    void compareScreenshots() throws IOException {
+        page.navigate("https://the-internet.herokuapp.com/");
+        Path actualPath = Paths.get("actual.png");
+        Path expectedPath = Paths.get("expected.png");
+        Path diffPath = Paths.get("diff.png");
+        page.screenshot(new Page.ScreenshotOptions()
+                .setPath(actualPath));
+
+        long mismatchPosition;
+        try {
+            mismatchPosition = Files.mismatch(actualPath, expectedPath);
+            assertEquals(-1, mismatchPosition, "Файлы должны быть идентичны");
+        } catch (AssertionError e) {
+            BufferedImage expectedImage = ImageComparisonUtil.readImageFromResources(expectedPath.toString());
+            BufferedImage actualImage = ImageComparisonUtil.readImageFromResources(actualPath.toString());
+
+            ImageComparison imageComparison = new ImageComparison(expectedImage, actualImage, diffPath.toFile());
+            ImageComparisonResult result = imageComparison.compareImages();
+            BufferedImage diffImage = result.getResult();
+            ImageComparisonUtil.saveImage(diffPath.toFile(), diffImage);
+
+            float diffPercent = result.getDifferencePercent();
+            System.out.println("Процент отличающихся пикселей: " + diffPercent);
+            throw new AssertionError("Скриншоты различаются. Процент различий: " + diffPercent +
+                    "% . Diff-файл сохранён в: " + diffPath.toAbsolutePath(), e);
+        }
+
+    }
+
+};

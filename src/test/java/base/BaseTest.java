6c92d001ef618c677e44edf22775ae8a7fba7d17
impala667 <musermasha@gmail.com>
2026-03-04 13:47:39 +0300

    Практическое задание №3

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -14,7 +14,7 @@ public class BaseTest {
     void setUp() {
         playwright = Playwright.create();
         browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
-                .setHeadless(false)
+                .setHeadless(true)
                 .setSlowMo(1500));
         context = browser.newContext(new Browser.NewContextOptions()
                 .setRecordVideoDir(Paths.get("videos/")));
